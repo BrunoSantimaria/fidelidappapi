3f891b6eacf02572a6db3cb6259b7c297a33a4ec
Bruno Santimaria <santimariabruno@gmail.com>
2024-10-25 13:24:41 -0300

    arreglado formato

--- logger/logger.js
+++ logger/logger.js
@@ -1,32 +1,30 @@
 // utils/logger.js
-const Log = require('./Logger.model');
-const Slack = require('@slack/bolt');
+const Log = require("./Logger.model");
+const Slack = require("@slack/bolt");
 
 const app = new Slack.App({
-    signingSecret: process.env.SLACK_SIGNING_SECRET,
-    token: process.env.SLACK_BOT_TOKEN,
-})
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+  token: process.env.SLACK_BOT_TOKEN,
+});
 
+exports.contact = async (req, res) => {
+  try {
+    const { name, email, message, phone, organization } = req.body;
 
-const logAction = async (email, action, details) => {
-    console.log("SlackChannel",  process.env.SLACK_CHANNEL)
-    try {
-    const log = new Log({ email, action, details });
-    await log.save();
+    // Formatear detalles
+    const details = `
+      Nombre: ${name}
+      Email: ${email}
+      Organización: ${organization}
+      Teléfono: ${phone}
+      Mensaje: ${message}
+    `;
 
-    app.client.chat.postMessage({
-        channel: process.env.SLACK_CHANNEL,
-        text: `Nueva Accion Registrada en Fidelidapp
-        Email ${email} 
-        Accion: ${action} 
-        Detalle: ${details}`
-      })
-
-    console.log('Action logged:', log);
-    return log;
-
+    const contact = await log.logAction(email, "contact", details);
+    res.status(201).json({ message: "Mensaje enviado con éxito" });
   } catch (error) {
-    console.error('Error logging action:', error);
+    console.error("Error al enviar el mensaje:", error);
+    res.status(500).json({ error: "Error interno del servidor" });
   }
 };
 
