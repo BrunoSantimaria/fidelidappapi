d1759b2c997a5460fb5fb1c7d72c674cde409023
Alvaro  Villena <alvarovillena@Alvaros-MacBook-Air.local>
2024-11-05 13:21:36 -0300

    leadparser email 0.2

--- utils/leadsemailparser.js
+++ utils/leadsemailparser.js
@@ -9,17 +9,39 @@ const client = require('twilio')(accountSid, authToken);
 
 router.post("/", async (req, res) => {
     console.log('Correo recibido en leadsemailparser');
-
-    //const { from, subject, text } = req.body;
+    console.log('Req Body:' + req.body);
 
     try {
+
+        const toAddress = req.body.to; // Store the 'to' address
+        const fromAddress = req.body.from;
+        const subject = req.body.subject;
+        const textMessage = req.body.text;
+        const business = toAddress.split('@')[0]; // This will give you "parser.example.com"
+
+        // Define the message template
+        const messageToSend = `
+        Hola ${business},
+
+        Ha llegado un nuevo formulario de contacto.
+
+        From: ${fromAddress}
+        Subject: ${subject}
+        Message: ${textMessage}
+    `;
+
+        console.log('To address:', toAddress);
+        console.log('Message:', messageToSend);
+
         client.messages
             .create({
                 from: 'whatsapp:+14155238886',
-                contentSid: 'HXb5b62575e6e4ff6129ad7c8efe1f983e',
-                contentVariables: '{"1":"12/1","2":"3pm"}',
+                //contentSid: 'HX80bd66de6cc4c5e97c2cf0e74ae551a6',
+                //contentVariables: '{"1":"Fidelidapp","2":"3pm"}',
+                body: messageToSend,
                 to: 'whatsapp:+56996706983'
             })
+            .then((message) => console.log(message.sid))
 
         res.status(200).send('Lead procesado y notificación enviada');
     } catch (error) {
@@ -32,3 +54,5 @@ router.post("/", async (req, res) => {
 
 
 module.exports = router;
+
+
