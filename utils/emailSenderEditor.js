194ff971a652c71d10fe005c9d671ec87a9d7071
Bruno Santimaria <santimariabruno@gmail.com>
2024-11-15 18:12:55 -0300

    asd

--- utils/emailSenderEditor.js
+++ utils/emailSenderEditor.js
@@ -3,25 +3,36 @@ sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 const sendMarketingEmailEditor = async ({ to, subject, template, from }) => {
   try {
-    // Validar que la plantilla se haya proporcionado
     if (!template || typeof template !== "string") {
       throw new Error("Template is required and must be a valid string.");
     }
 
-    // Crear el objeto de mensaje
+    // Agregar el enlace de desuscripción al final del template
+    const templateWithUnsubscribe = `
+      ${template}
+      <div style="margin-top: 20px; text-align: center;">
+        <p style="font-size: 12px; color: #666;">
+          Si no deseas recibir más correos, puedes 
+          <a href="<%asm_group_unsubscribe_raw_url%>">desuscribirte aquí</a>
+        </p>
+      </div>
+    `;
+
     const msg = {
       to,
       from: from,
       subject,
-      html: template, // Aquí se usa el template que le pasas como argumento
+      html: templateWithUnsubscribe,
+      asm: {
+        group_id: 32167,
+      },
     };
 
-    // Enviar el correo utilizando SendGrid
     await sgMail.send(msg);
     console.log("Email sent successfully.");
   } catch (error) {
     console.error("Error sending email:", error);
-    throw error; // Lanzamos el error para que lo maneje el controlador
+    throw error;
   }
 };
 
